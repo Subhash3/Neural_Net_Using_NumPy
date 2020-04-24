a5b2c0b3e52221c7225c3df9d0b0576c0d4b2256
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-04-24 15:48:22 +0530

    Separate class for Activation functions

---
+++ Activation_Function.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+class ActivationFunction() :
+    def __init__(self, function_name) :
+        if function_name == 'sigmoid' :
+            self.activation_function = self._sigmoid
+        elif function_name == 'tanh' :
+            self.activation_function = self._tanh
+        elif function_name == 'relu':
+            self.activation_function = self._relu
+        elif function_name == 'softmax':
+            self.activation_function = self._softmax
+
+    def activate(self, x, derivative=False) :
+        if derivative :
+            return self.activation_function(x, derivative=True)
+        return self.activation_function(x)
+
+    def _sigmoid(self, x, derivative=False) :
+        if derivative :
+            return x * (1 - x)
+        return 1/(1 + np.exp(-x))
+
+    def _tanh(self, x, derivative=False) :
+        return 2/(1 + np.exp(-2*x)) -1
+
+    def _relu(self, x, derivative=False) :
+        return np.max(0,x)
+
+    def _softmax(self, x, derivative=False) :
+        return np.exp(x)/np.sum(np.exp(x))
--- Activation_Functions.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-class ActivationFunction() :
-    def __init__(self, function_name) :
-        pass

Fixed Delta calculation bug

Layer.py
@@ -12,29 +12,33 @@ class Layer() :
         self.activation_function = activation_function
 
     def feed(self, input_array) :
-        dot_product = np.dot(self.weights, input_array.transpose())
+        # print("Weights", self.weights.shape)
+        # print("Inputs ", input_array.shape)
+        dot_product = np.dot(self.weights, input_array)
         dot_product += self.biases
+        # print("Output: ", dot_product.shape)
         return self.activate(dot_product)
 
     def activate(self, x) :
         activator = ActivationFunction(self.activation_function)
         return activator.activate(x)
 
-    def calculate_gradients(self, target_or_weights, layer_type="hidden", next_layer_deltas=None) :
+    def calculate_gradients(self, target_or_weights, layer_type, next_layer_deltas=None) :
         activator = ActivationFunction(self.activation_function)
         if layer_type == "output" :
-            # Output Layer
+            # print("Output Layer")
             # target_or_weights = target values
             self.deltas = (target_or_weights - self.outputs) * activator.activate(self.outputs, derivative=True)
             # delta = (target - output) * activation_derivative(output)
         else :
-            # Hidden Layer
+            # print("Hidden Layer")
             # target_or_weights = Next layer's weights
             self.deltas = np.dot(target_or_weights.transpose(), next_layer_deltas) * activator.activate(self.outputs, derivative=True)
             # deltas += (next_layers weights * next_layers deltas)
     
     def update_weights(self, inputs, learningRate) :
-        self.weights += (self.deltas * learningRate * inputs)
+        change_in_weights = (self.deltas * learningRate) * inputs.transpose()
+        self.weights = self.weights + change_in_weights
         self.biases += (self.biases * learningRate)
 
     def display(self) :
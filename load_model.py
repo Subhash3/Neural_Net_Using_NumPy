47bcbb23288c544e1e1a426aebb0319bf78d141d
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-05-01 13:03:08 +0530

    Adding load model usage files

---
+++ load_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from src.Model import NeuralNetwork
+import numpy as np
+
+model_file = "./xor-model.json"
+network = NeuralNetwork.load_model(model_file)
+
+input_array = np.array([[0], [0]])
+output = network.predict(input_array)
+print("Input: ", input_array.T)
+print("Output: ", output.T)
+
+network.epoch_vs_error()
+network.evaluate()

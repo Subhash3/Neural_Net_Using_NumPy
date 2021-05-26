Fixed gradient descent issues

nicenet/Layer.py
@@ -72,8 +72,8 @@ class Layer:
 
     def update_weights(self, inputs, learningRate):
         change_in_weights = np.dot(self.deltas, inputs.T) * learningRate
-        self.weights = np.add(self.weights, change_in_weights)
-        self.biases += self.deltas * learningRate
+        self.weights = np.subtract(self.weights, change_in_weights)
+        self.biases = np.subtract(self.biases, self.deltas * learningRate)
 
     def display(self):
         print("\tInputs: ", self.inputs)

nicenet/LossFunctions.py
@@ -31,8 +31,8 @@ class LossFunctions:
         # print("hihi")
         error = np.subtract(targets, outputs)
         if derivative:
-            return error
-        return error*error
+            return -1*error
+        return np.mean(error*error)
 
     def _cross_entropy(self, outputs, targets, derivative=False):
         error = np.subtract(targets, outputs)
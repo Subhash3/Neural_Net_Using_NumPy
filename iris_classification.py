4d73dce6526a1cea0f9cb868be5894a86faa22a8
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-05-26 22:41:24 +0530

    Added iris model

--- iris_classification.py
+++ iris_classification.py
@@ -8,8 +8,8 @@ import numpy as np
 inputs = 4
 outputs = 3
 network = NeuralNetwork(inputs, outputs)
-network.addLayer(10, activation_function="sigmoid")
-network.addLayer(10, activation_function="sigmoid")
+network.addLayer(8, activation_function="sigmoid")
+network.addLayer(8, activation_function="sigmoid")
 network.compile(activation_function="softmax")
 network.setLearningRate(0.1)
 
@@ -18,11 +18,13 @@ datasetHandler.makeDataset('./datasets/Iris/inputs.csv', './datasets/Iris/target
 data, size = datasetHandler.getRawData()
 data = shuffleArray(data)
 training, testing = splitArr(data, 3/4)
-print(len(training))
+# print(len(training))
 
-network.Train(training, len(training), epochs=500)
+network.Train(training, len(training), epochs=50, logging=False)
 network.evaluate()
 network.epoch_vs_error()
+
+network.export_model('iris_model.json')
 
 correct = 0
 total = 0
@@ -36,4 +38,19 @@ for sample in testing :
     if p == a:
         correct += 1
     total += 1
-print(correct*100/total)
+print("Testing accuracy:", correct*100/total)
+
+# newNetwork = NeuralNetwork.load_model('iris_model.json')
+# correct = 0
+# total = 0
+# for sample in testing :
+#     features = sample[0]
+#     prediction = newNetwork.predict(features)
+#     actual = sample[1]
+#     p = np.argmax(prediction)
+#     a = np.argmax(actual.T[0])
+
+#     if p == a:
+#         correct += 1
+#     total += 1
+# print("Testing accuracy:", correct*100/total)

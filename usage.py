2a102b94b0e97e3e7421668d0b66cb5d748257f0
Anyesh <sthacruz@gmail.com>
2021-06-02 22:33:13 +0545

    fix: Incorporate usage with new changes

--- usage.py
+++ usage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from nicenet import NeuralNetwork
-from nicenet import Dataset
+# from nicenet import Dataset
 import numpy as np
+
+from nicenet import NeuralNetwork
 
 XOR_data = [
     (np.array([[0], [0]]), np.array([[0]])),
@@ -21,13 +22,13 @@ size = 4
 # XOR_data, size = datasetCreator.getRawData()
 
 network = NeuralNetwork(2, 1, cost="ce")
-network.setLearningRate(0.1)
-network.addLayer(8, activation_function="tanh")
-network.addLayer(8, activation_function="tanh")
-network.addLayer(8, activation_function="tanh")
-network.addLayer(8, activation_function="tanh")
+network.set_learning_rate(0.1)
+network.add_layer(8, activation_function="tanh")
+network.add_layer(8, activation_function="tanh")
+network.add_layer(8, activation_function="tanh")
+network.add_layer(8, activation_function="tanh")
 network.compile(activation_function="sigmoid")
-network.Train(XOR_data, size, epochs=5, logging=False)
+network.fit(XOR_data, size, epochs=5, logging=False)
 # network.epoch_vs_error()
 network.evaluate()
 

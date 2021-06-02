274c07124f79803b54441e4ce908dfae892e023b
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-02 19:08:45 +0530

    Converted regex vars to uppercase

--- docstrings-to-markdown/regex.py
+++ docstrings-to-markdown/regex.py
@@ -1,6 +1,6 @@
 # Extract class or function along with its docstring
 # Docstring will be captured in group 1 and group 0 contains the whole class or function along with its docstring.
-def_with_docs_regex = r'((def|class).*((\s*->\s*.*)|):\n\s*"""(\n\s*.*?)*""")'
+DEF_WITH_DOCS_REGEX = r'((def|class).*((\s*->\s*.*)|):\n\s*"""(\n\s*.*?)*""")'
 
 # Given a docstring, identity each part, i.e, parameters and return values.
 # Note: This regex works only with numpy style docstrings.
@@ -8,4 +8,4 @@ def_with_docs_regex = r'((def|class).*((\s*->\s*.*)|):\n\s*"""(\n\s*.*?)*""")'
 # Description           1
 # Parameters            3
 # Returns               6
-identify_each_part_regex = r'"""\n\s*(.*\s*)*?(Parameters\s*-*\s*((.*\s*)*?))?(Returns\s*-*\s*(.*\s*)*?)?"""'
+IDENTIFY_EACH_PART_REGEX = r'"""\n\s*(.*\s*)*?(Parameters\s*-*\s*((.*\s*)*?))?(Returns\s*-*\s*(.*\s*)*?)?"""'

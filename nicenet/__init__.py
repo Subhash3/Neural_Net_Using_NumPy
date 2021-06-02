a88608ebf3acee040a7728d4aeaa06b728398ca5
Anyesh <sthacruz@gmail.com>
2021-06-02 22:32:57 +0545

    fix: Add noqa for unused imports

--- nicenet/__init__.py
+++ nicenet/__init__.py
@@ -1,4 +1,7 @@
 # from .ActivationFunction import ActivationFunction
-from .Dataset import Dataset
-from .NeuralNetwork import NeuralNetwork
+from .Dataset import Dataset  # noqa
+from .NeuralNetwork import NeuralNetwork  # noqa
+
 # from .Layer import Layer
+
+__all__ = ["Dataset", "NeuralNetwork"]

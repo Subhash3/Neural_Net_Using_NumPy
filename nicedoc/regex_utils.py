2fca2aa8e465b7098fb34ee89756ca81f1266d28
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-03 15:26:03 +0530

    Tweaked the camel-case to snake-case regex

--- nicedoc/regex_utils.py
+++ nicedoc/regex_utils.py
@@ -11,4 +11,4 @@ DEF_WITH_DOCS_REGEX = r'((def|class).*((\s*->\s*.*)|):\n\s*"""(\n\s*.*?)*""")'
 IDENTIFY_EACH_PART_REGEX = r'"""\n\s*(.*\s*)*?(Parameters\s*-*\s*((.*\s*)*?))?(Returns\s*-*\s*(.*\s*)*?)?"""'
 
 # Given a python file, extracts all the camelcased variable/methods/functions.
-EXTRACT_CAMEL = r"(\.|\s|^)(\b[a-z]+)(([A-Z]+[a-z]+)+?)\b"
+EXTRACT_CAMEL = r"(^(?!\s*print.*).*)((\b[a-z]+)(([A-Z]+[a-z]+)+?)\b)"

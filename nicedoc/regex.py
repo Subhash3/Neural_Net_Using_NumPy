6eece20641acde90e9ab44c1090cef1724aa0122
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-03 09:19:06 +0530

    Added camel-case regex

--- nicedoc/regex.py
+++ nicedoc/regex.py
@@ -9,3 +9,6 @@ DEF_WITH_DOCS_REGEX = r'((def|class).*((\s*->\s*.*)|):\n\s*"""(\n\s*.*?)*""")'
 # Parameters            3
 # Returns               6
 IDENTIFY_EACH_PART_REGEX = r'"""\n\s*(.*\s*)*?(Parameters\s*-*\s*((.*\s*)*?))?(Returns\s*-*\s*(.*\s*)*?)?"""'
+
+# Given a python file, extracts all the camelcased variable/methods/functions.
+EXTRACT_CAMEL = r"(\.|\s|^)(\b[a-z]+)(([A-Z]+[a-z]+)+?)\b"
